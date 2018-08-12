5af86b152cfc61afc91b75548625aad7e65cba34
Tea Green <4a1h0012@gmail.com>
2018-08-12 19:08:19 +0800

    Initial Commit

---
+++ RecordFoodApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecordFoodApp
+//
+//  Created by mac on 2018/8/12.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
